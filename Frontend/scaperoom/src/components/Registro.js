104be172a571c65e4bd720d289ffd136143d4d63
FernandoTiradosG <fernando.tiradosgonzalez.web@adaits.es>
2024-06-13 18:41:31 +0200

    Ultimos cambios

--- Frontend/scaperoom/src/components/Registro.js
+++ Frontend/scaperoom/src/components/Registro.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import './CSS/Registro.css';
 
 function Registro() {
   const [email, setEmail] = useState('');
